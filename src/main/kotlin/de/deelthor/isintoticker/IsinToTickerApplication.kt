f86f62971cb0cc2f4afccf6a618e9671dff1d9e4
Thorsten Deelmann <thorsten.deelmann@codecentric.de>
2021-10-15 17:14:19 +0200

    initial import

---
+++ src/main/kotlin/de/deelthor/isintoticker/IsinToTickerApplication.kt
@@ -0,0 +1,11 @@
+package de.deelthor.isintoticker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IsinToTickerApplication
+
+fun main(args: Array<String>) {
+    runApplication<IsinToTickerApplication>(*args)
+}
---
+++ src/test/kotlin/de/deelthor/isintoticker/IsinToTickerApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.deelthor.isintoticker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IsinToTickerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
